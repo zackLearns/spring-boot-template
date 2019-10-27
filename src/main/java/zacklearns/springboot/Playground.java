a6c1492811f87c7b5b1c45f1a92ab180f22cc4c3
Zack <zack.learns@gmail.com>
2019-10-27 16:47:50 +0800

    Initial commit of working spring boot application

---
+++ src/main/java/zacklearns/springboot/Playground.java
@@ -0,0 +1,17 @@
+package zacklearns.springboot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Playground {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Playground.class, args);
+    }
+}
